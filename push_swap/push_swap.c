a3b89539772513b7da193f5accb62ef00ac785b5
Leo Zylberman-nasrallah <lzylberm@shi-r9-p7.s19.be>
2021-10-22 11:26:50 +0200

    push swap modified

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: lzylberm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 16:34:00 by lzylberm          #+#    #+#             */
-/*   Updated: 2021/10/21 18:48:58 by lzylberm         ###   ########.fr       */
+/*   Updated: 2021/10/22 11:25:53 by lzylberm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,36 +21,195 @@ void	print_list(t_list *lst)
 	}
 }
 
-void	sa(t_list *stack_a)
+void	sa(t_list **stack_a)
 {
-	t_list	*tmp;
+	t_list	*tmp1;
+	t_list	*tmp2;
 	
-	tmp = NULL;
-	if (ft_lstsize(stack_a) > 1)
+	tmp1 = NULL;
+	tmp2 = NULL;
+	if (ft_lstsize(*stack_a) > 1)
 	{
-		tmp = stack_a->next->next;
-		stack_a->next = stack_a->next->next;;
-		stack_a->next->next = stack_a;
-		free (tmp);
+		tmp1 = *stack_a;
+		tmp2 = *stack_a;
+		tmp1 = tmp1->next;
+		*stack_a = tmp1;
+		tmp2->next = tmp1->next;
+		tmp1->next = tmp2;
+	}
+}
+
+void	sb(t_list **stack_b)
+{
+	t_list	*tmp1;
+	t_list	*tmp2;
+
+	tmp1 = NULL;
+	tmp2 = NULL;
+	if (ft_lstsize(*stack_b) > 1)
+	{
+		tmp1 = *stack_b;
+		tmp2 = *stack_b;
+		tmp1 = tmp1->next;
+		*stack_b = tmp1;
+		tmp2->next = tmp1->next;
+		tmp1->next = tmp2;
+	}
+}
+
+void	ss(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*tmp1;
+	t_list	*tmp2;
+
+	tmp1 = NULL;
+	tmp2 = NULL;
+	if (ft_lstsize(*stack_a) > 1)
+	{
+		tmp1 = *stack_a;
+		tmp2 = *stack_a;
+		tmp1 = tmp1->next;
+		*stack_a = tmp1;
+		tmp2->next = tmp1->next;
+		tmp1->next = tmp2;
+	}
+	if (ft_lstsize(*stack_b) > 1)
+	{
+		tmp1 = *stack_b;
+		tmp2 = *stack_b;
+		tmp1 = tmp1->next;
+		*stack_b = tmp1;
+		tmp2->next = tmp1->next;
+		tmp1->next = tmp2;
+	}
+}
+
+void	pa(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*tmp1;
+	t_list	*tmp2;
+
+	if (*stack_b)
+	{
+		tmp1 = *stack_b;
+		tmp2 = tmp1->next;
+		*stack_b = tmp2;
+		tmp2 = *stack_a;
+		*stack_a = tmp1;
+		tmp1->next = tmp2;
+	}
+}
+
+void	pb(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*tmp1;
+	t_list	*tmp2;
+	
+	if (*stack_a)
+	{
+		tmp1 = *stack_a;
+		tmp2 = tmp1->next;
+		*stack_a = tmp2;
+		tmp2 = *stack_b;
+		*stack_b = tmp1;
+		tmp1->next = tmp2;
+	}
+}
+
+void	ra(t_list **stack_a)
+{
+	t_list	*tmp1;
+	t_list	*tmp2;
+	
+	if (ft_lstsize(*stack_a) > 1)
+	{
+		tmp1 = *stack_a;
+		*stack_a = tmp1->next;
+		tmp2 = ft_lstlast(*stack_a);
+		tmp2->next = tmp1;
+		tmp1->next = NULL;
+	}
+}
+
+void	rb(t_list **stack_b)
+{
+	t_list	*tmp1;
+	t_list	*tmp2;
+
+	if (ft_lstsize(*stack_b) > 1)
+	{
+		tmp1 = *stack_b;
+		*stack_b = tmp1->next;
+		tmp2 = ft_lstlast(*stack_b);
+		tmp2->next = tmp1;
+		tmp1->next = NULL;
+	}
+}
+
+void	rr(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*tmp1;
+	t_list	*tmp2;
+
+	if (ft_lstsize(*stack_a) > 1)
+	{
+		tmp1 = *stack_a;
+		*stack_a = tmp1->next;
+		tmp2 = ft_lstlast(*stack_a);
+		tmp2->next = tmp1;
+		tmp1->next = NULL;
+	}
+	if (ft_lstsize(*stack_b) > 1)
+	{
+		tmp1 = *stack_b;
+		*stack_b = tmp1->next;
+		tmp2 = ft_lstlast(*stack_b);
+		tmp2->next = tmp1;
+		tmp1->next = NULL;
 	}
 }
 
 int	main(int argc, char **argv)
 {
 	t_list	*stack_a;
-//	t_list	*stack_b;
+	t_list	*stack_b;
 	int		i;
 
 	i = 2;
 	stack_a = ft_lstnew(ft_atoi(argv[1]));
+	stack_b = ft_lstnew(8);
 	while(i < argc)
 	{
 		ft_lstadd_back(&stack_a, ft_lstnew(ft_atoi(argv[i])));
 		i++;
 	}
+	printf("stack A : ");
 	print_list(stack_a);
 	printf("\n");
-	sa(stack_a);
+	printf("swap A\n");
+	sa(&stack_a);
+	printf("stack A : ");
+	print_list(stack_a);
+	printf("\n");
+	printf("push B\n");
+	pb(&stack_a, &stack_b);
+	printf("stack A : ");
+	print_list(stack_a);
+	printf("\n");
+	printf("stack B : ");
+	print_list(stack_b);
+	printf("\n");
+	printf("push A\n");
+	pa(&stack_a, &stack_b);
+	printf("stack A : ");
+	print_list(stack_a);
+	printf("\n");
+	printf("stack B : ");
+	print_list(stack_b);
+	printf("\n");
+	printf("rotate A\n");
+	ra(&stack_a);
+	printf("stack A : ");
 	print_list(stack_a);
 
 	return (0);
